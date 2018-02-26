bc1db689e6ccef90a908945264d41e0be605cea6
Ryan Rieger <rmr6894@rit.edu>
2018-02-26 15:23:19 -0500

    Added team logo and exit button. Buttons now become lighter when the mouse is over them.

--- Beaulax/Beaulax/Game1.cs
+++ Beaulax/Beaulax/Game1.cs
@@ -20,21 +20,36 @@ namespace Beaulax
         SpriteBatch spriteBatch;
         Vector2 initialPosition;
 
+        #region Main Menu
         // main menu textures and attributes
         Texture2D mainMenuBackground;
         Texture2D newGameButton;
         Texture2D loadGameButton;
         Texture2D optionsButton;
+        Texture2D exitButton;
+        Texture2D teamName;
+        bool newGameButtonHover = false;
+        bool loadGameButtonHover = false;
+        bool optionsButtonHover = false;
+        bool exitButtonHover = false;
         int buttonWidth;
         int buttonHeight;
         int buttonX;
         int newGameButtonY;
         int loadGameButtonY;
         int optionsButtonY;
+        int teamNameWidth;
+        int teamNameHeight;
+        int teamNameX;
+        int teamNameY;
+        int exitButtonDimension;
+        int exitButtonX;
+        int exitButtonY;
+        #endregion
 
         // screen size attributes
-        int screenWidth = 1200;
-        int screenHeight = 720;
+        int screenWidth = 1920;
+        int screenHeight = 1080;
         double widthScaleFactor = 1;
         double heightScaleFactor = 1;
 
@@ -92,6 +107,8 @@ namespace Beaulax
             newGameButton = Content.Load<Texture2D>("Main Menu Assets/NewGame");
             loadGameButton = Content.Load<Texture2D>("Main Menu Assets/LoadGame");
             optionsButton = Content.Load<Texture2D>("Main Menu Assets/Options");
+            exitButton = Content.Load<Texture2D>("Main Menu Assets/Exit");
+            teamName = Content.Load<Texture2D>("Main Menu Assets/ColdHot Studios");
         }
 
         /// <summary>
@@ -193,27 +210,88 @@ namespace Beaulax
         {
             MouseState mState = Mouse.GetState();
 
-            // check if the mouse is in the X position that the buttons are
+            // check if the mouse is in the X position that the buttons are located at
             if (mState.X >= buttonX && mState.X <= buttonX + buttonWidth)
             {
                 // start new game when new game button is left clicked
-                if (mState.Y >= newGameButtonY && mState.Y <= newGameButtonY + buttonHeight && mState.LeftButton == ButtonState.Pressed)
+                if (mState.Y >= newGameButtonY && mState.Y <= newGameButtonY + buttonHeight)
                 {
-                    currentState = GameState.Gameplay;
+                    // set to true when the mouse is over the button
+                    newGameButtonHover = true;
+
+                    // if the left mouse button is clicked then switch states
+                    if (mState.LeftButton == ButtonState.Pressed)
+                    {
+                        currentState = GameState.Gameplay;
+                    }
+                }
+                // set to false when the mouse is not over the button
+                else
+                {
+                    newGameButtonHover = false;
                 }
 
                 // load the saved game when load game button is left clicked
-                if (mState.Y >= loadGameButtonY && mState.Y <= loadGameButtonY + buttonHeight && mState.LeftButton == ButtonState.Pressed)
+                if (mState.Y >= loadGameButtonY && mState.Y <= loadGameButtonY + buttonHeight)
                 {
-                    saver.Load(player);
-                    currentState = GameState.Gameplay;
+                    // set to true when the mouse is over the button
+                    loadGameButtonHover = true;
+
+                    // if the left mouse button is clicked then load the save and switch states
+                    if (mState.LeftButton == ButtonState.Pressed)
+                    {
+                        saver.Load(player);
+                        currentState = GameState.Gameplay;
+                    }
+                }
+                // set to false when the mouse is not over the button
+                else
+                {
+                    loadGameButtonHover = false;
                 }
 
                 // open the options menu when the options button is left clicked
-                if (mState.Y >= optionsButtonY && mState.Y <= optionsButtonY + buttonHeight && mState.LeftButton == ButtonState.Pressed)
+                if (mState.Y >= optionsButtonY && mState.Y <= optionsButtonY + buttonHeight)
                 {
-                    currentState = GameState.Options;
+                    // set to true when the mouse is over the button
+                    optionsButtonHover = true;
+
+                    // if the left mouse button is clicked then switch states
+                    if (mState.LeftButton == ButtonState.Pressed)
+                    {
+                        currentState = GameState.Options;
+                    }
                 }
+                // set to false when the mouse is not over the button
+                else
+                {
+                    optionsButtonHover = false;
+                }
+            }
+            // set to false when the mouse is not over the button
+            else
+            {
+                newGameButtonHover = false;
+                loadGameButtonHover = false;
+                optionsButtonHover = false;
+            }
+
+            // open the options menu when the options button is left clicked
+            if (mState.Y >= exitButtonY && mState.Y <= exitButtonY + exitButtonDimension && mState.X >= exitButtonX && mState.X <= exitButtonX + exitButtonDimension)
+            {
+                // set to true when the mouse is over the button
+                exitButtonHover = true;
+
+                // if the left mouse button is clicked then exit the game
+                if (mState.LeftButton == ButtonState.Pressed)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            // set to false when the mouse is not over the button
+            else
+            {
+                exitButtonHover = false;
             }
         }
 
@@ -225,7 +303,7 @@ namespace Beaulax
         {
             IsMouseVisible = true;
 
-            // calculate scale factors for appropriately resizing depending on screen resolution
+            // calculate scale factors for appropriately resizing buttons depending on screen resolution
             widthScaleFactor = (float)mainMenuBackground.Width / screenWidth;
             heightScaleFactor = (float)mainMenuBackground.Height / screenHeight;
 
@@ -237,13 +315,48 @@ namespace Beaulax
             buttonX = (screenWidth - buttonWidth) / 2;
             newGameButtonY = (int)(screenHeight / (12f / 7f));
             loadGameButtonY = (int)(newGameButtonY + buttonHeight + screenHeight * (1f / 72f));
-            optionsButtonY = (int)(loadGameButtonY + buttonHeight + screenHeight * (1f / 72f));
+            optionsButtonY = loadGameButtonY + 2 * buttonHeight;
 
+            // calculate team name size
+            teamNameWidth = (int)(screenWidth * (91f / 600f));
+            teamNameHeight = (int)(screenHeight * (1f / 12f));
+
+            // calculate team name location
+            teamNameX = (int)(screenWidth * (1f / 80f));
+            teamNameY = (int)(screenHeight - screenHeight * (1f / 48f) - teamNameHeight);
+
+            // calculate exit button size
+            exitButtonDimension = (int)(screenWidth * (1f / 24f));
+
+            // calculate exit button location
+            exitButtonX = (int)(screenWidth - screenWidth * (1f / 120f) - exitButtonDimension);
+            exitButtonY = (int)(screenHeight * (1f / 72f));
+            
             // draw the main menu
             spriteBatch.Draw(mainMenuBackground, new Rectangle(0, 0, screenWidth, screenHeight), Color.White);
-            spriteBatch.Draw(newGameButton, new Rectangle(buttonX, newGameButtonY, buttonWidth, buttonHeight), Color.White);
-            spriteBatch.Draw(loadGameButton, new Rectangle(buttonX, loadGameButtonY, buttonWidth, buttonHeight), Color.White);
-            spriteBatch.Draw(optionsButton, new Rectangle(buttonX, optionsButtonY, buttonWidth, buttonHeight), Color.White);
+            spriteBatch.Draw(newGameButton, new Rectangle(buttonX, newGameButtonY, buttonWidth, buttonHeight), Color.Gray);
+            spriteBatch.Draw(loadGameButton, new Rectangle(buttonX, loadGameButtonY, buttonWidth, buttonHeight), Color.Gray);
+            spriteBatch.Draw(optionsButton, new Rectangle(buttonX, optionsButtonY, buttonWidth, buttonHeight), Color.Gray);
+            spriteBatch.Draw(teamName, new Rectangle(teamNameX, teamNameY, teamNameWidth, teamNameHeight), Color.White);
+            spriteBatch.Draw(exitButton, new Rectangle(exitButtonX, exitButtonY, exitButtonDimension, exitButtonDimension), Color.Gray);
+
+            // if the mouse is over a button then make that button lighter
+            if (newGameButtonHover)
+            {
+                spriteBatch.Draw(newGameButton, new Rectangle(buttonX, newGameButtonY, buttonWidth, buttonHeight), Color.White);
+            }
+            if (loadGameButtonHover)
+            {
+                spriteBatch.Draw(loadGameButton, new Rectangle(buttonX, loadGameButtonY, buttonWidth, buttonHeight), Color.White);
+            }
+            if (optionsButtonHover)
+            {
+                spriteBatch.Draw(optionsButton, new Rectangle(buttonX, optionsButtonY, buttonWidth, buttonHeight), Color.White);
+            }
+            if (exitButtonHover)
+            {
+                spriteBatch.Draw(exitButton, new Rectangle(exitButtonX, exitButtonY, exitButtonDimension, exitButtonDimension), Color.White);
+            }
         }
         #endregion
 
