06375c35c0d9891639540d9980fe7802a771458f
coletrain9903 <coleboggan@hotmail.com>
2018-03-06 00:28:20 -0500

    Started the basis for creating platforms

--- Beaulax/Beaulax/Classes/Obstacles.cs
+++ Beaulax/Beaulax/Classes/Obstacles.cs
@@ -12,5 +12,19 @@ namespace Beaulax.Classes
 {
     class Obstacles: StaticObjects
     {
+        // attributes
+        private Rectangle hitBox;
+
+        // parameters
+        public Rectangle HitBox { get { return hitBox; } set { hitBox = value; } }
+
+        // constructor
+        public Obstacles(int iWidth, int iHeight, Vector2 iLocation)
+        {
+            this.width = iWidth;
+            this.height = iHeight;
+            this.location = iLocation;
+            hitBox = new Rectangle((int)iLocation.X, (int)iLocation.Y, width, height);
+        }
     }
 }
--- Beaulax/Beaulax/Classes/StaticObjects.cs
+++ Beaulax/Beaulax/Classes/StaticObjects.cs
@@ -12,5 +12,14 @@ namespace Beaulax.Classes
 {
     class StaticObjects: GameObjects
     {
+        // attributes
+        protected Vector2 location; // the vector which will hold the collidable object
+        protected int width; // width of the object
+        protected int height; // height of the object
+
+        // properties
+        public Vector2 Location { get { return location; } set { location = value; } }
+        public int Width { get { return width; } set { width = value; } }
+        public int Height { get { return height; } set { height = value; } }
     }
 }
