4204d6ac1155562ef8839329d0673a4289e34c45
laurenavilla <36425709+laurenavilla@users.noreply.github.com>
2018-04-26 17:15:16 -0400

    Made/Formatted Narratives class
    - added attributes
    - made it inherit GameObjects for location and size
    - added method shells needed for formatting and drawing text onto the screen

--- Beaulax/Beaulax/Classes/Narratives.cs
+++ Beaulax/Beaulax/Classes/Narratives.cs
@@ -10,7 +10,38 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Beaulax.Classes
 {
-    /*class Narratives: Collectibles
+    class Narratives : GameObjects
     {
-    }*/
+        // attributes
+        private string narrativeText;
+        private SpriteFont font;
+
+        // constructor
+        public Narratives(string narrative)
+        {
+            narrativeText = narrative;
+        }
+
+        // methods
+        /// <summary>
+        /// Format story piece to fit dialogue box
+        /// </summary>
+        /// <returns></returns>
+        public void formatNarrative()
+        {
+            
+        }
+
+        /// <summary>
+        /// Draws story in box to screen
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="n"></param>
+        public void DrawStory(SpriteBatch spriteBatch, Narratives n)
+        {
+            spriteBatch.Begin();
+            // spriteBatch.DrawString(); // add font
+            spriteBatch.End();
+        }
+    }
 }
